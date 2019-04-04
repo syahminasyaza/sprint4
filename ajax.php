0403b8a6b9378a7f5e522d0731b10d68b246f630
syahminasyaza <49135521+syahminasyaza@users.noreply.github.com>
2019-04-04 10:38:08 +0800

    Add files via upload

---
+++ ajax.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("db.php");
+
+$db_handle = new DBController();
+
+	if(!empty($_POST["state_id_2"])) {
+
+		$query ="SELECT * FROM city WHERE state_id_2 = '" . $_POST["state_id_2"] . "' ORDER BY city";
+		$results = $db_handle->runQuery($query);
+
+?>
+
+		<option value="">Select City</option>
+
+<?php
+
+		foreach($results as $city) {
+
+?>
+
+		<option value="<?php echo $city["city_id"].$city["cityID"]; ?>"><?php echo $city["city"]; ?></option>
+
+<?php
+	}
+}
+?>
